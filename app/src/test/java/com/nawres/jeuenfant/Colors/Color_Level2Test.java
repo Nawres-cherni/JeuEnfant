cdcae2308e9f59ef731d917cfe5c5f8a7c31aaf2
Nawres-cherni <cherninawress22@gmail.com>
2021-01-11 12:41:12 +0100

    settings

---
+++ app/src/test/java/com/nawres/jeuenfant/Colors/Color_Level2Test.java
@@ -0,0 +1,25 @@
+package com.nawres.jeuenfant.Colors;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Color_Level2Test {
+
+    @Test
+    public void onCreate() {
+
+    }
+
+    @Test
+    public void addListenerOnButton() {
+    }
+
+    @Test
+    public void levelColor() {
+    }
+
+    @Test
+    public void help() {
+    }
+}
---
+++ app/src/test/java/com/nawres/jeuenfant/SignInTest.java
@@ -0,0 +1,20 @@
+package com.nawres.jeuenfant;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class SignInTest {
+    private static final String Fake_String ="Succes Login";
+@Mock
+Context mMockContext;
+
+    @Test
+    public void onCreate() {
+
+
+    }
+}
